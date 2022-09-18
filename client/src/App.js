010965d49c328ed4986ac9fb55108d294636d6ca
schavan6 <schavan6@student.gsu.edu>
2022-09-17 23:41:00 -0400

    code cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -5,13 +5,6 @@ import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
-import ProfileForm from './components/profile-forms/ProfileForm';
-import AddExperience from './components/profile-forms/AddExperience';
-import AddEducation from './components/profile-forms/AddEducation';
-import Profiles from './components/profiles/Profiles';
-import Profile from './components/profile/Profile';
-import Posts from './components/posts/Posts';
-import Post from './components/post/Post';
 import NotFound from './components/layout/NotFound';
 import PrivateRoute from './components/routing/PrivateRoute';
 import { LOGOUT } from './actions/types';
@@ -92,8 +85,6 @@ const App = () => {
             <Route path="/" element={<Landing />} />
             <Route path="register" element={<Register />} />
             <Route path="login" element={<Login />} />
-            <Route path="profiles" element={<Profiles />} />
-            <Route path="profile/:id" element={<Profile />} />
             <Route
               path="/forms"
               element={
@@ -109,27 +100,6 @@ const App = () => {
                   <RoomPage user={user} socket={socket} users={users} />
                 </PrivateRoute>
               }
-            />
-            <Route
-              path="create-profile"
-              element={<PrivateRoute component={ProfileForm} />}
-            />
-            <Route
-              path="edit-profile"
-              element={<PrivateRoute component={ProfileForm} />}
-            />
-            <Route
-              path="add-experience"
-              element={<PrivateRoute component={AddExperience} />}
-            />
-            <Route
-              path="add-education"
-              element={<PrivateRoute component={AddEducation} />}
-            />
-            <Route path="posts" element={<PrivateRoute component={Posts} />} />
-            <Route
-              path="posts/:id"
-              element={<PrivateRoute component={Post} />}
             />
             <Route path="/*" element={<NotFound />} />
           </Routes>
